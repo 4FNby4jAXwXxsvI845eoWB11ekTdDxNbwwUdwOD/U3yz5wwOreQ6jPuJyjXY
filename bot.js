8ec566be325f66f1b873e6e8f1ef43c6a82f4628
4FNby4jAXwXxsvI845eoWB11ekTdDxNbwwUdwOD <45238840+4FNby4jAXwXxsvI845eoWB11ekTdDxNbwwUdwOD@users.noreply.github.com>
2018-11-23 14:12:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,15 @@
-﻿const Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "1";
 
 client.on('ready', () => {
-  console.log(`...`);  
 });
 
 client.on('ready', async() => {
 var server = "514822456998101002"; //
-var channel = "514822457706807312";//
+var channel = "514838309785174026";//
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein hussein**')
     },305);
-})
-
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "say") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
  });
 
  
